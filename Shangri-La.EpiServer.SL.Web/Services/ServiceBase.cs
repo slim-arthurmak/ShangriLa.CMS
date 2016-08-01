b0688f1f9182279fb6b32c4c4a80152bee001bae
mike man <manchunyip@gmail.com>
2016-08-01 05:55:56 +0000

    Add WeatherService for current tempature and time.

---
+++ Shangri-La.EpiServer.SL.Web/Services/ServiceBase.cs
@@ -0,0 +1,151 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+using log4net;
+
+namespace Shangri_La.EpiServer.SL.Web.Services
+{
+    public class ServiceBase
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(ServiceBase));
+
+        protected Stream CreateJsonResponse(object response, bool format = false)
+        {
+            try
+            {
+                WebOperationContext.Current.OutgoingResponse.ContentType = "application/json";
+                var jsonString = JsonConvert.SerializeObject(response, format ? Formatting.Indented : Formatting.None);
+                var stream = new MemoryStream(Encoding.UTF8.GetBytes(jsonString));
+                return stream;
+            }
+            catch (Exception ex)
+            {
+                log.ErrorFormat("CreateJsonResponse: FAILED  message='{0}' stack was {1}",
+                    /*0*/ ex.Message,
+                    /*1*/ ex.StackTrace);
+
+                return null;
+            }
+        }
+
+        protected dynamic ReadJsonRequest(string request)
+        {
+            return ReadJsonRequestInternal(request);
+        }
+
+        protected dynamic ReadJsonRequest(Stream request)
+        {
+            using (var streamReader = new StreamReader(request))
+            {
+                var requestString = streamReader.ReadToEnd();
+
+                return ReadJsonRequestInternal(requestString);
+            }
+        }
+
+        /// <summary>
+        ///     HTML encode before processing request. Restore quotes, because these are part of the JSON definition
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns></returns>
+        private dynamic ReadJsonRequestInternal(string request)
+        {
+            var encodedRequest = HttpUtility.HtmlEncode(request);
+            encodedRequest = encodedRequest.Replace("&quot;", "\"");
+
+            dynamic jsonObject = JObject.Parse(encodedRequest);
+            return jsonObject;
+        }
+
+        /// <summary>
+        ///     Sets content language to the language code that was sent in the accept-language HTTP header.
+        ///     (If this where a CMS application, like EPiServer, then we would se the CMS language for this request)
+        ///     NOTE that this SHOULD use .net and /or ISO locale (YBD)
+        ///     short ==  .NET culture == ISO Locale
+        ///     EN == en-HK  ==  en_HK = English (Hong Kong SAR China)"
+        ///     ZH == zh-CN  ==  zh_Hans_CN = "Chinese (Simplified Han, China)",
+        /// </summary>
+        /// <param name="langOverride"> Ovverides the HTTP request header setting</param>
+        //protected GlobalHelper.Language SetContentLanguage(string langOverride)
+        //{
+        //    var language = GlobalHelper.Language.EN;
+        //    char[] DelimiterChars = { ',', ';' };
+        //    var or = false;
+        //    var found = false;
+
+        //    try
+        //    {
+        //        var requestLanguage = HttpContext.Current.Request.Headers["accept-language"];
+        //        if (!string.IsNullOrWhiteSpace(langOverride))
+        //        {
+        //            foreach (GlobalHelper.Language lang in Enum.GetValues(typeof(GlobalHelper.Language)))
+        //            {
+        //                foreach (var l in GlobalHelper.LanguageISOvariants[lang])
+        //                {
+        //                    if (langOverride.Equals(l, StringComparison.InvariantCultureIgnoreCase))
+        //                    {
+        //                        language = lang;
+        //                        or = true;
+        //                        log.DebugFormat("SetContentLanguage(): Override language to {0}",
+        //                            language.ToString().ToLower());
+        //                        break;
+        //                    }
+        //                }
+        //                if (or) break;
+        //            }
+        //        }
+
+        //        if (!or)
+        //        {
+        //            if (!string.IsNullOrEmpty(requestLanguage))
+        //            {
+        //                var languages = requestLanguage.Split(DelimiterChars, StringSplitOptions.RemoveEmptyEntries);
+
+        //                foreach (GlobalHelper.Language lang in Enum.GetValues(typeof(GlobalHelper.Language)))
+        //                {
+        //                    foreach (var l in GlobalHelper.LanguageISOvariants[lang])
+        //                    {
+        //                        var index = Array.FindIndex(languages, x => x.ToLower() == l.ToString().ToLower());
+        //                        if (index != -1)
+        //                        {
+        //                            language = lang;
+        //                            found = true;
+        //                            break;
+        //                        }
+        //                    }
+        //                    if (found) break;
+        //                }
+        //                if (!found)
+        //                    log.WarnFormat(
+        //                        "SetContentLanguage(): 'accept-language' request header {0} does not contain GlobalHelper.Language value ",
+        //                        requestLanguage);
+        //            }
+        //            else
+        //            {
+        //                log.DebugFormat("SetContentLanguage(): 'accept-language' request header not found ");
+        //            }
+        //        }
+        //        WebOperationContext.Current.OutgoingResponse.Headers.Add("Content-Language",
+        //            GlobalHelper.LanguageISOvariants[language][0]);
+        //        HttpContext.Current.Items["ContentLanguage"] = GlobalHelper.LanguageISOvariants[language][0];
+
+        //        log.DebugFormat("SetContentLanguage(): Added {0} to 'Content-Language'",
+        //            GlobalHelper.LanguageISOvariants[language][0]);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        log.ErrorFormat("SetContentLanguage(): EXCEPTION message='{0}' stack was {1}",
+        //            /*0*/ ex.Message,
+        //            /*1*/ ex.StackTrace);
+        //    }
+        //    return language;
+        //}
+
+    }
+}
---
+++ Shangri-La.EpiServer.SL.Web/Services/WeatherService.svc.cs
@@ -0,0 +1,256 @@
+﻿using EPiServer;
+using EPiServer.Core;
+using EPiServer.ServiceLocation;
+using EPiServer.Web;
+using log4net;
+using Newtonsoft.Json;
+using Shangri_La.EpiServer.SL.Web.Business;
+using Shangri_La.EpiServer.SL.Web.Models.Blocks;
+using Shangri_La.EpiServer.SL.Web.Models.Pages;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Security;
+using System.Runtime.Serialization;
+using System.Security.Cryptography.X509Certificates;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Shangri_La.EpiServer.SL.Web.Services
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class WeatherService : ServiceBase
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(WeatherService));
+
+        private static Injected<ContentLocator> contentLocator { get; set; }
+
+        // To use HTTP GET, add [WebGet] attribute. (Default ResponseFormat is WebMessageFormat.Json)
+        // To create an operation that returns XML,
+        //     add [WebGet(ResponseFormat=WebMessageFormat.Xml)],
+        //     and include the following line in the operation body:
+        //         WebOperationContext.Current.OutgoingResponse.ContentType = "text/xml";
+        [OperationContract]
+        [AspNetCacheProfile("CacheServiceWeatherService")]
+        //[WebGet(UriTemplate = "/GetCurrentWeatherJSON/?hotelCode={hotelCode}&cityCode={cityCode}&lat={latitude}&lon={longitude}", ResponseFormat = WebMessageFormat.Json)]
+        [WebGet(UriTemplate = "/GetCurrentWeatherJSON/?hotelCode={hotelCode}&cityCode={cityCode}", ResponseFormat = WebMessageFormat.Json)]
+        public WeatherRawData GetCurrentWeatherJSON(string hotelCode, string cityCode)
+        {
+            var hotelBlocks = contentLocator.Service.GetHotelBlocks() as List<HotelBlock>;
+            HotelBlock hotelBlock = hotelBlocks.Where(h => h.HotelCode == hotelCode).FirstOrDefault();
+
+            if (hotelBlock != null)
+            {
+                string output = GetWeatherJsonFunc(hotelBlock.Latitude, hotelBlock.Longitude);
+
+                if (!string.IsNullOrEmpty(output))
+                {
+                    try
+                    {
+                        JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings();
+                        jsonSerializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
+                        WeatherRawData wrd = JsonConvert.DeserializeObject<WeatherRawData>(output, jsonSerializerSettings);
+                        if (wrd != null && wrd.weather != null)
+                        {
+                            return wrd;
+                        }
+                    }
+                    catch { }
+                }
+            }
+
+            return new WeatherRawData();
+            //return new List<string>() { "test", "testb", hotelCode, cityCode };
+
+        }
+
+        [OperationContract]
+        //[AspNetCacheProfile("CacheServiceWeatherService")]
+        [WebGet(UriTemplate = "/GetTimeZoneJSON/?hotelCode={hotelCode}&cityCode={cityCode}&lat={latitude}&lon={longitude}&timestamp={timestamp}", ResponseFormat = WebMessageFormat.Json)]
+        public TimeZoneRawData GetTimeZoneJSON(string latitude, string longitude, string hotelCode, string cityCode, string timestamp)
+        {
+            string output = GetTimeZoneJsonFunc(latitude, longitude, timestamp);
+
+            if (!string.IsNullOrEmpty(output))
+            {
+                try
+                {
+                    JsonSerializerSettings jsonSerializerSettings = new JsonSerializerSettings();
+                    jsonSerializerSettings.MissingMemberHandling = MissingMemberHandling.Ignore;
+                    TimeZoneRawData trd = JsonConvert.DeserializeObject<TimeZoneRawData>(output, jsonSerializerSettings);
+                    if (trd != null && trd.timeZoneId != null)
+                    {
+                        return trd;
+                    }
+                }
+                catch { }
+            }
+
+            return new TimeZoneRawData() { timeZoneId = "Asia/Shanghai" };
+
+        }
+
+
+        private string GetWeatherJsonFunc(string latitude, string longitude)
+        {
+            string resultString = string.Empty;
+
+            try
+            {
+                string weatherInfoUrl = ConfigurationManager.AppSettings["WeatherInfoUrl"];
+                int weatherInfoTimeout = 1000; // default 1sec
+                int.TryParse(ConfigurationManager.AppSettings["WeatherInfoTimeout"], out weatherInfoTimeout);
+
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(ValidateServerCertificate);
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11;
+                WebRequest request = (HttpWebRequest)WebRequest.Create(string.Format(weatherInfoUrl, latitude, longitude));
+                request.Method = "GET";
+                request.Timeout = weatherInfoTimeout;
+
+                // get response
+                using (var response = request.GetResponse())
+                {
+                    using (var responseStream = response.GetResponseStream())
+                    {
+                        StreamReader streamReader = new StreamReader(responseStream);
+                        resultString = streamReader.ReadToEnd();
+                        //... clear the result if it contains error
+                        if (string.IsNullOrWhiteSpace(resultString))
+                        {
+                            resultString = string.Empty;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Logger.Error(this, "error in getting Weather Info ", ex);
+                resultString = string.Empty;
+            }
+
+            return resultString;
+        }
+
+
+        private string GetTimeZoneJsonFunc(string latitude, string longitude, string timestamp)
+        {
+            string resultString = string.Empty;
+
+            try
+            {
+                string timeZoneInfoUrl = ConfigurationManager.AppSettings["TimeZoneInfoUrl"];
+                int timeZoneInfoTimeout = 1000; // default 1sec
+                int.TryParse(ConfigurationManager.AppSettings["TimeZoneInfoTimeout"], out timeZoneInfoTimeout);
+
+                ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(ValidateServerCertificate);
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11;
+                WebRequest request = (HttpWebRequest)WebRequest.Create(string.Format(timeZoneInfoUrl, latitude, longitude, timestamp));
+                request.Method = "GET";
+                request.Timeout = timeZoneInfoTimeout;
+
+                // get response
+                using (var response = request.GetResponse())
+                {
+                    using (var responseStream = response.GetResponseStream())
+                    {
+                        StreamReader streamReader = new StreamReader(responseStream);
+                        resultString = streamReader.ReadToEnd();
+                        //... clear the result if it contains error
+                        if (string.IsNullOrWhiteSpace(resultString))
+                        {
+                            resultString = string.Empty;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Logger.Error(this, "error in getting Weather Info ", ex);
+                resultString = string.Empty;
+            }
+
+            return resultString;
+        }
+
+        public static bool ValidateServerCertificate(Object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+
+        //public Stream GetWeatherJSON(string param)
+        //{
+        //    try
+        //    {
+        //        Parameter parameter = JsonConvert.DeserializeObject<Parameter>(param);
+        //        //List<ExperienceViewModel> lstExperiences = ExperienceHelper.GetExperienceLists(parameter);
+
+        //        //return CreateJsonResponse(lstExperiences, GlobalHelper.DoPrintableJson());
+
+        //        List<string> testList = new List<string>();
+        //        return CreateJsonResponse(testList);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        log.ErrorFormat("GetExperiencesJSON() : EXCEPTION   message='{0}' stack  {1}",
+        //            /*1*/ ex.Message,
+        //            /*2*/ ex.StackTrace);
+        //    }
+
+        //    return null;
+        //}
+    }
+
+    // Add more operations here and mark them with [OperationContract]
+
+    public class Weather
+    {
+        public int id { get; set; }
+        public string main { get; set; }
+        public string description { get; set; }
+        public string icon { get; set; }
+    }
+
+    public class Main
+    {
+        public int humudity { get; set; }
+        public int pressure { get; set; }
+        public string temp { get; set; }
+        public string temp_max { get; set; }
+        public string temp_min { get; set; }
+    }
+
+    public class Coord
+    {
+        public string lon { get; set; }
+        public string lat { get; set; }
+    }
+
+    public class WeatherRawData
+    {
+        public string cod { get; set; }
+        public long dt { get; set; }
+        public Coord coord { get; set; }
+        public Main main { get; set; }
+        public List<Weather> weather { get; set; }
+    }    
+
+    public class TimeZoneRawData
+    {
+        public string timeZoneId { get; set; }
+    }
+
+    //public class Parameter
+    //{
+    //    public string Latitude { get; set; }
+
+    //    public string Longitude { get; set; }
+
+    //}
+}
